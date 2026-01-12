e202dbf81bd832f505b125be56c7c3e10f315564
Rasel Ahmmed <rahmmed@mtrx-node03pd.dcm.senecacollege.ca>
2026-01-11 23:31:58 -0500

    Added Workshop Zero source code

---
+++ Workshops/w0/w0.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "#### Workshop Zero ####" << endl;
+    cout << "Subject and Section:" << endl;
+    cout << "OOP244 NCC" << endl;
+    cout << "Name:" << endl;
+    cout << "Rasel Ahmmed" << endl;
+    cout << "Student Number:" << endl;
+    cout << "182801233" << endl;
+    cout << "Email:" << endl;
+    cout << "rahmmed@myseneca.ca" << endl;
+    return 0;
+}
+
