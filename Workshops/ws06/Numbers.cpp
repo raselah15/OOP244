687848f2d7c5e8e0f2d744f0052a3b2722928a14
Rasel Ahmmed <rahmmed@myseneca.ca>
2026-03-09 14:25:56 -0400

    fix workshop 6 numbers.cpp

--- Workshops/ws06/Numbers.cpp
+++ Workshops/ws06/Numbers.cpp
@@ -1,6 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
-#include <iomanip>
 #include <cstring>
 #include "Numbers.h"
 
@@ -8,183 +8,184 @@ using namespace std;
 
 namespace seneca {
 
+bool Numbers::isEmpty() const {
+   return m_numbers == nullptr;
+}
+
+void Numbers::setEmpty() {
+   m_numbers = nullptr;
+   m_filename = nullptr;
+   m_numCount = 0;
+}
+
+void Numbers::setFilename(const char* filename) {
+   delete[] m_filename;
+   m_filename = new char[strlen(filename) + 1];
+   strcpy(m_filename, filename);
+}
+
+size_t Numbers::numberCount() const {
+   ifstream file(m_filename);
+   size_t count = 0;
+   double temp;
+
+   while (file >> temp) {
+      count++;
+   }
+
+   return count;
+}
+
+void Numbers::sort() {
+   for (size_t i = 0; i < m_numCount; i++) {
+      for (size_t j = i + 1; j < m_numCount; j++) {
+         if (m_numbers[i] > m_numbers[j]) {
+            double temp = m_numbers[i];
+            m_numbers[i] = m_numbers[j];
+            m_numbers[j] = temp;
+         }
+      }
+   }
+}
+
+bool Numbers::load() {
+   ifstream file(m_filename);
+   if (!file) return false;
+
+   m_numbers = new double[m_numCount];
+
+   for (size_t i = 0; i < m_numCount; i++) {
+      file >> m_numbers[i];
+   }
+
+   return true;
+}
+
+void Numbers::save() const {
+   if (isEmpty() || !m_isOriginal) return;
+
+   ofstream file(m_filename);
+
+   for (size_t i = 0; i < m_numCount; i++) {
+      file << m_numbers[i] << endl;
+   }
+}
+
+Numbers::Numbers() {
+   setEmpty();
+   m_isOriginal = false;
+}
+
+Numbers::Numbers(const char* filename) {
+   setEmpty();
+   m_isOriginal = true;
+
+   setFilename(filename);
+
+   m_numCount = numberCount();
+
+   if (m_numCount > 0) {
+
+      if (load())
+         sort();
+      else {
+         delete[] m_numbers;
+         delete[] m_filename;
+         setEmpty();
+         m_isOriginal = false;
+      }
+   }
+}
+
+Numbers::Numbers(const Numbers& src) {
+   setEmpty();
+   *this = src;
+}
+
 Numbers::~Numbers() {
    save();
    delete[] m_numbers;
    delete[] m_filename;
 }
 
-Numbers::Numbers(const Numbers& other) {
-   setEmpty();
-   m_isOriginal = false;
-
-   if (!other.isEmpty()) {
-      m_numCount = other.m_numCount;
-      m_numbers = new double[m_numCount];
-
-      for (size_t i = 0; i < m_numCount; i++)
-         m_numbers[i] = other.m_numbers[i];
-   }
-}
-
-Numbers& Numbers::operator=(const Numbers& other) {
-
-   if (this != &other) {
+Numbers& Numbers::operator=(const Numbers& src) {
+   if (this != &src) {
 
       delete[] m_numbers;
+      delete[] m_filename;
 
-      m_numCount = other.m_numCount;
+      m_numCount = src.m_numCount;
+      m_isOriginal = false;
 
-      if (other.m_numbers) {
-         m_numbers = new double[m_numCount];
-
-         for (size_t i = 0; i < m_numCount; i++)
-            m_numbers[i] = other.m_numbers[i];
+      if (src.m_filename) {
+         m_filename = new char[strlen(src.m_filename) + 1];
+         strcpy(m_filename, src.m_filename);
       }
-      else
-         m_numbers = nullptr;
+
+      if (src.m_numbers) {
+         m_numbers = new double[m_numCount];
+         for (size_t i = 0; i < m_numCount; i++) {
+            m_numbers[i] = src.m_numbers[i];
+         }
+      }
    }
 
    return *this;
-}
-
-size_t Numbers::numberCount() const {
-
-   size_t count = 0;
-
-   ifstream file(m_filename);
-   char ch;
-
-   while (file.get(ch))
-      if (ch == '\n')
-         count++;
-
-   return count;
-}
-
-bool Numbers::load() {
-
-   bool success = false;
-
-   if (m_numCount > 0) {
-
-      m_numbers = new double[m_numCount];
-
-      ifstream file(m_filename);
-
-      size_t i = 0;
-
-      while (file && i < m_numCount) {
-
-         file >> m_numbers[i];
-
-         if (file)
-            i++;
-      }
-
-      if (i == m_numCount)
-         success = true;
-      else {
-         delete[] m_numbers;
-         setEmpty();
-      }
-   }
-
-   return success;
-}
-
-void Numbers::save() const {
-
-   if (m_isOriginal && !isEmpty()) {
-
-      ofstream file(m_filename);
-
-      for (size_t i = 0; i < m_numCount; i++)
-         file << m_numbers[i] << endl;
-   }
 }
 
 Numbers& Numbers::operator+=(double value) {
 
-   if (!isEmpty()) {
+   double* temp = new double[m_numCount + 1];
 
-      double* temp = new double[m_numCount + 1];
-
-      for (size_t i = 0; i < m_numCount; i++)
-         temp[i] = m_numbers[i];
-
-      m_numCount++;
-
-      temp[m_numCount - 1] = value;
-
-      delete[] m_numbers;
-
-      m_numbers = temp;
-
-      sort();
+   for (size_t i = 0; i < m_numCount; i++) {
+      temp[i] = m_numbers[i];
    }
+
+   temp[m_numCount] = value;
+
+   delete[] m_numbers;
+   m_numbers = temp;
+
+   m_numCount++;
+
+   sort();
 
    return *this;
 }
 
-ostream& Numbers::display(ostream& ostr) const {
+ostream& Numbers::display(ostream& os) const {
 
    if (isEmpty()) {
-
-      ostr << "Empty list";
-
+      os << "Empty list";
    }
    else {
 
-      ostr << fixed << setprecision(2);
-
-      ostr << "=========================" << endl;
-
-      if (m_isOriginal)
-         ostr << m_filename << endl;
-      else
-         ostr << "*** COPY ***" << endl;
+      os << "=========================" << endl;
 
       for (size_t i = 0; i < m_numCount; i++) {
-
-         ostr << m_numbers[i];
-
-         if (i != m_numCount - 1)
-            ostr << ", ";
+         os << m_numbers[i] << endl;
       }
 
-      ostr << endl;
-
-      ostr << "-------------------------" << endl;
-
-      ostr << "Total of " << m_numCount << " number(s)" << endl;
-
-      ostr << "Largest number:  " << max() << endl;
-
-      ostr << "Smallest number: " << min() << endl;
-
-      ostr << "Average:         " << average() << endl;
-
-      ostr << "=========================";
+      os << "-------------------------" << endl;
+      os << "Total numbers: " << m_numCount << endl;
+      os << "Average: " << average() << endl;
+      os << "Minimum: " << min() << endl;
+      os << "Maximum: " << max() << endl;
+      os << "=========================" << endl;
    }
 
-   return ostr;
+   return os;
 }
 
 ostream& operator<<(ostream& os, const Numbers& N) {
    return N.display(os);
 }
 
-istream& operator>>(istream& istr, Numbers& N) {
-
+istream& operator>>(istream& is, Numbers& N) {
    double value;
-
-   istr >> value;
-
-   if (istr)
-      N += value;
-
-   return istr;
+   is >> value;
+   N += value;
+   return is;
 }
 
 }
